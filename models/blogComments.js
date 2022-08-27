b62a938a2d933bbbd6bcfcb8113f7ff01cd7b8d1
Adam Martin <adammartinmac@adams-air.lan>
2022-08-27 14:35:16 -0400

    Retooled post model for password model

--- models/blogComments.js
+++ models/blogComments.js
@@ -1,5 +1,47 @@
-const blogUsers = require('models/blogUsers.js');
-const blogPost = require('models/blogPosts.js');
-const blogComments = require('models/blogComments.js');
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('config/connection.js');
 
-module.exports = {blogUsers, blogPosts, blogComments};
+class blogComments extends Model {};
+
+// Model initialization
+blogComments.init(
+    {
+        // Unique key for all posts
+        postID: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+
+        // Connects the postID to the userID 
+        postUserID: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id',
+            }
+        },
+
+        // Post title
+        postTitle: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+        },
+
+        // Post body content
+        postContents: {
+            type: DataTypes.TEXT,
+            allowNull:false,
+        }
+
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'post'
+    }
+);
+
+module.exports = blogPosts;
---
+++ models/blogUsers.js
@@ -0,0 +1,61 @@
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt'); // Required for password encryption
+const sequelize = require('config/connection.js');
+
+class blogUsers extends Model {
+    // Function for password validation comparing entry to password stored in user data
+    blogPassCheck(userPass) {
+        return bcrypt.compareSync(userPass, this.password) //Pass or fail condition
+    }
+};
+
+// Model initialization
+blogUsers.init(
+    {
+        // Unique key for all users
+        userID: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+
+        // Username storage 
+        userName: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+
+        // Password storage
+        userPass: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+
+        // Email storage
+        userEmail: {
+            type: DataTypes.STRING,
+            allowNull:false,
+            validate: {
+                isEmail: true,
+            },
+        },
+    },
+    {
+        // Hook for utilizing encryption functionality of bcrypt on user password
+        hooks: {
+            beforeCreate: async (userValidation) => {
+              userValidation.password = await bcrypt.hash(userValidation.password, 10);
+              return userValidation;  
+            },
+        },
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'user'
+    }
+);
+
+module.exports = blogUsers;
