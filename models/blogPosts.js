926862349a970817798a785c3f8a409b5b85b812
Adam Martin <adammartinmac@adams-air.lan>
2022-08-27 14:15:50 -0400

    Re-tooled blogPosts.js model from wk 13 homework example

---
+++ models/blogPosts.js
@@ -0,0 +1,47 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('config/connection.js');
+
+class blogPosts extends Model {};
+
+// Model initialization
+blogPosts.init(
+    {
+        // Unique key for all posts
+        postID: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+
+        // Connects the postID to the userID 
+        postUserID: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id',
+            }
+        },
+
+        // Post title
+        postTitle: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+        },
+
+        // Post body content
+        postContents: {
+            type: DataTypes.TEXT,
+            allowNull:false,
+        }
+
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'post'
+    }
+);
+
+module.exports = blogPosts;
