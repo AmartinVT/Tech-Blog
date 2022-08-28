c93d22e163dcec50f9256972a2fd6864451f757a
Adam Martin <adammartinmac@adams-air.lan>
2022-08-28 15:59:21 -0400

    commented out server.js lines to allow backend to run

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const session = require('express-session');
 const exphbs = require('express-handlebars');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-const routes = require('./controllers');
+//const routes = require('./controllers');
 const sequelize = require('./config/connection');
-const helpers = require('./utils/helpers');
+//const helpers = require('./utils/helpers');
 
 const app = express();
 const PORT = process.env.PORT || 3006;
@@ -26,17 +26,17 @@ const sess = {
 
 app.use(session(sess));
 
-const hbs = exphbs.create({ helpers });
+//const hbs = exphbs.create({ helpers });
 
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+//app.engine('handlebars', hbs.engine);
+//app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, "node_modules/bootstrap/dist/")));
 
-app.use(routes);
+//app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () =>
