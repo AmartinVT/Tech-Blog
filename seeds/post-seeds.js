b3b8d9c46df4863c686a9f0fba65789eb6ba273b
Adam Martin <adammartinmac@adams-air.lan>
2022-08-28 17:37:24 -0400

    Fixed more seed vs model field name errors

--- seeds/post-seeds.js
+++ seeds/post-seeds.js
@@ -3,37 +3,37 @@ const { Posts } = require('../models');
 const postSeed = [
     {
         postTitle: 'testPostTitle1 - user1',
-        postContent: 'testPostContent1 - user1',
+        postContents: 'testPostContent1 - user1',
         userID: 1,
     },
     
     {
         postTitle: 'testPostTitle2 - user1',
-        postContent: 'testPostContent2 - user1',
+        postContents: 'testPostContent2 - user1',
         userID: 1,
     },
     
     {
         postTitle: 'testPostTitle3 - user2',
-        postContent: 'testPostContent3 - user2',
+        postContents: 'testPostContent3 - user2',
         userID: 2,
     },
 
     {
         postTitle: 'testPostTitle4 - user2',
-        postContent: 'testPostContent4 - user2',
+        postContents: 'testPostContent4 - user2',
         userID: 2,
     },
 
     {
         postTitle: 'testPostTitle5 - user3',
-        postContent: 'testPostContent5 - user3',
+        postContents: 'testPostContent5 - user3',
         userID: 3,
     },
 
     {
         postTitle: 'testPostTitle6 - user3',
-        postContent: 'testPostContent6 - user3',
+        postContents: 'testPostContent6 - user3',
         userID: 3,
     },
 ];
